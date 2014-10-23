1f91898ea25a3fbf164aed0cb4677131c267dbe5
raphael goujet <raphael.goujet@gmail.com>
2014-10-24 00:53:44 +0200

    Un décor avec des collisionneurs fonctionnels ; 2 joueurs dont 1 fonctionnel avec les flèches du clavier

---
+++ Assets/Scripts/ControleDuJoueur.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControleDuJoueur : MonoBehaviour {
+
+	private Vector3 _inputMovement;
+	public float absoluteMultiplier;
+	
+	// Update is called once per frame
+	void Update () {
+		_inputMovement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);		
+		//Vector3 moveAmount = _inputMovement * currentMoveSpeed;		
+		//this.rigidbody2D.AddForce(moveAmount);
+
+		this.transform.localPosition += binariserVecteur(_inputMovement)*absoluteMultiplier;
+	}
+
+
+
+	Vector3 binariserVecteur(Vector3 vector)
+	{
+      return new Vector3(binariser(vector.x), binariser(vector.y), 0);
+	}
+
+	float seuil = 0f;
+	float binariser(float f)
+	{
+      if(Mathf.Abs(f) - seuil > 0)
+		{
+			return Mathf.Sign(f);
+		}
+		else
+		{
+			return 0;
+		}
+	}
+}
---
+++ Assets/Scripts/PositionneurDeCubes.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionneurDeCubes : MonoBehaviour {
+
+	public GameObject cubeHautGauche;
+	public GameObject cubeHautDroite;
+	public GameObject cubeBasGauche;
+
+	// Use this for initialization
+	void Start () {
+
+		Vector3 horizontal = cubeHautDroite.transform.localPosition - cubeHautGauche.transform.localPosition;
+		Vector3 vertical = cubeBasGauche.transform.localPosition - cubeHautGauche.transform.localPosition;
+		
+		for (int i = 1; i <= 5; i++)
+		{
+			for (int j = 1; j <= 6; j++)
+			{
+				GameObject nouveauCube = Instantiate(cubeHautGauche) as GameObject;
+				nouveauCube.transform.localPosition = nouveauCube.transform.localPosition + (i-1)*vertical + (j-1)*horizontal;
+				nouveauCube.transform.parent = gameObject.transform;
+			}
+		}
+
+		cubeHautGauche.SetActive(false);
+		cubeHautDroite.SetActive(false);
+		cubeBasGauche.SetActive(false);
+	}
+}
