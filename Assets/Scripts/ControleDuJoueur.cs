dbb341152ee95364d449804a87749c1f4f0d5a11
raphael goujet <raphael.goujet@gmail.com>
2014-10-24 01:17:26 +0200

    implémentation rapide pose bombes

--- Assets/Scripts/ControleDuJoueur.cs
+++ Assets/Scripts/ControleDuJoueur.cs
@@ -3,18 +3,45 @@ using System.Collections;
 
 public class ControleDuJoueur : MonoBehaviour {
 
-	private Vector3 _inputMovement;
 	public float absoluteMultiplier;
-
 	public int numeroJoueur;
+	public GameObject bombeModele;
+	public int reserveDeBombes;
+
+	private Vector3 _inputMovement;
+	private string nomControleHorizontal;
+	private string nomControleVertical;
+	private KeyCode toucheBombe;
+
+	void Start()
+	{
+		nomControleHorizontal = "Horizontal"+numeroJoueur;
+		nomControleVertical = "Vertical"+numeroJoueur;
+
+		if(numeroJoueur == 1)
+		{
+			toucheBombe = KeyCode.RightShift;
+		}
+		else
+		{
+			toucheBombe = KeyCode.LeftShift;
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		_inputMovement = new Vector3(Input.GetAxis("Horizontal"+numeroJoueur), Input.GetAxis("Vertical"+numeroJoueur), 0);		
+		_inputMovement = new Vector3(Input.GetAxis(nomControleHorizontal), Input.GetAxis(nomControleVertical), 0);		
 		//Vector3 moveAmount = _inputMovement * currentMoveSpeed;		
 		//this.rigidbody2D.AddForce(moveAmount);
 
 		this.transform.localPosition += binariserVecteur(_inputMovement)*absoluteMultiplier;
+
+		if(reserveDeBombes > 0 && Input.GetKey(toucheBombe))
+		{
+			GameObject bombe = Instantiate(bombeModele) as GameObject;
+			bombe.transform.localPosition = gameObject.transform.localPosition;
+			reserveDeBombes--;
+		}
 	}
 
 
